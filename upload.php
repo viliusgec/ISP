d5725e668928bb26b520036f37d8d765e29f4731
Erikas Masaitis <57370157+ErikasMasaitis@users.noreply.github.com>
2020-12-11 20:57:44 +0200

    nuotrauka to DB
    padarytas nuotraukos directory įkėlimas į DB

--- upload.php
+++ upload.php
@@ -2,15 +2,29 @@
 <?php
 
 session_start();
+include("database/database.class.php");
+$databaseObj = new database();
+$conn = $databaseObj->connect();
 
 if ($_FILES['fileToUpload']['size']>1048576) {
   $_SESSION['message_photo'] = "<h3 style='color: red'>Nuotraukos dydis per didelis</h3>";
   header("Location: photoUpload.php");
   die(); 
 }
+$user = $_SESSION['userID'];
+$photo = "./uploads/" . $user . $_FILES['fileToUpload']['name'];
 
-if (move_uploaded_file($_FILES['fileToUpload']['tmp_name'], "./uploads/" . $_SESSION['userID'] . $_FILES['fileToUpload']['name'])) {
+// galima padaryti, kad neleistų du kart įkelt nuotraukos tam pačiam useriui
+// kol neatmetė senos foto
+if (move_uploaded_file($_FILES['fileToUpload']['tmp_name'], $photo)) {
   $_SESSION['message_photo'] = "<h3 style='color: green'>Nuotrauka įkelta sėkmingai</h3>"; 
+   $sql = "INSERT INTO `nuotraukos` (`location`, `vartotojo_id`, `busena`)
+   VALUES ('$photo', '$user', '0')";
+        if ($conn->query($sql) === TRUE) {
+          echo "New record created successfully";
+        } else {
+          echo "Error: " . $sql . "<br>" . $conn->error;
+        }
   header("Location: photoUpload.php");
 } else {
   $_SESSION['message_photo'] = "<h3 style='color: red'>Nuotrauka neįkelta</h3>"; 
