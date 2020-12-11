fa10caba69c96e0b1ed08e2a2951d6b62beae199
Erikas Masaitis <57370157+ErikasMasaitis@users.noreply.github.com>
2020-12-11 23:15:51 +0200

    Update userdata.php

--- userdata.php
+++ userdata.php
@@ -5,17 +5,33 @@ include("main_bar.html");
   </nav>
  
   <div class="jumbotron text-center animate__animated animate__fadeIn">
-    <h1>Įkelkite savo nuotrauką</h1>
-    
-  <?php echo "" . $_SESSION['message_photo']; ?>
-    <form action="upload.php" method="post" enctype="multipart/form-data">
-        Pasirinkite nuotrauką įkėlimui:
-        <input type="file" name="fileToUpload" id="fileToUpload" value="Ieškoti..."></br>
-        <input class="btn btn-outline-dark" type="submit" value="Įkelkiti" name="submit">
-      </form>
-  
-   
+    <h2>Įveskite duomenis, kuriuos norite redaguoti:</h2>
+    <form action="procuserdata.php" method="post">
+    <div class="form-group">
+        <input type="text" class="form-control" id="vardas" placeholder="Įveskite naują vardą:">
+      </div>
 
+      <div class="form-group">
+        <input type="text" class="form-control" id="pavarde" placeholder="Įveskite naują pavardę:">
+      </div>
+
+      <div class="form-group">
+        <input type="email" class="form-control" id="vardas" placeholder="Įveskite naują el. paštą:">
+      </div>
+
+      <div class="form-group">
+        <input type="text" class="form-control" id="slaptazodis" placeholder="Įveskite naują slaptažodį:">
+      </div>
+    Šitas neveikia:
+      <div class="form-group">
+        <input type="text" class="form-control" id="slaptazodis" placeholder="Pakartokite naują slaptažodį:">
+      </div>
+
+      <div class="form-group">
+        <input type="number" class="form-control" id="asmens_kodas" placeholder="Įveskite naują asmens kodą:">
+      </div>
+      <input type="submit" class="btn btn-outline-danger" value="Patvirtinti"></input>
+    </form>
   </div>
  
  <?php 
