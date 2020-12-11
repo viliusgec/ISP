23e53dbff526216fd1fae930522f994ce871bde6
Erikas Masaitis <57370157+ErikasMasaitis@users.noreply.github.com>
2020-12-11 20:59:56 +0200

    įkelta foto
    komentarai, foto

--- upload.php
+++ upload.php
@@ -7,7 +7,7 @@ $databaseObj = new database();
 $conn = $databaseObj->connect();
 
 if ($_FILES['fileToUpload']['size']>1048576) {
-  $_SESSION['message_photo'] = "<h3 style='color: red'>Nuotraukos dydis per didelis</h3>";
+  $_SESSION['message_photo'] = "<h3 style='color: red'>Nuotraukos sveria per daug</h3>";
   header("Location: photoUpload.php");
   die(); 
 }
@@ -16,6 +16,7 @@ $photo = "./uploads/" . $user . $_FILES['fileToUpload']['name'];
 
 // galima padaryti, kad neleistų du kart įkelt nuotraukos tam pačiam useriui
 // kol neatmetė senos foto
+// ir dar padaryti, kad neleistų kelti nuotraukos, jeigu jau patvirtinta nuotrauka
 if (move_uploaded_file($_FILES['fileToUpload']['tmp_name'], $photo)) {
   $_SESSION['message_photo'] = "<h3 style='color: green'>Nuotrauka įkelta sėkmingai</h3>"; 
    $sql = "INSERT INTO `nuotraukos` (`location`, `vartotojo_id`, `busena`)
