065514ae02716be124c160c43b0504387aba982d
audrius133 <audriussz@gmail.com>
2020-12-14 19:00:03 +0200

    Darbuotojo editai
    Pridejau darbuotojo nav linka atskira tik darbuotojam. ir bsk stiliukus pakeiciau

---
+++ worker.php
@@ -0,0 +1,25 @@
+<?php 
+session_start();
+include("main_bar.html");
+ ?>
+ 
+  <div class="jumbotron text-center">
+    <h1>Darbuotojo nustatymai</h1>
+    <br>
+
+    <a href="workerMessage.php" class="btn btn-outline-info">Zinuciu siuntimas</a>
+    <a href="workerGroupEdit.html" class="btn btn-outline-info">Teorijos grupiu redagavimas</a>
+    <a href="workerDrivingEdit.html" class="btn btn-outline-info">Vairavimu redagavimas</a>
+    <a href="calendar.html" class="btn btn-outline-info">Tvarkarastis</a>
+  
+   
+
+  </div>
+ 
+  
+  <button type="button" class="btn btn-danger live_chat_button">Live chat</button>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
+</html>
